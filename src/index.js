f07006a9b353c293390d249d8c67a35545fcbe44
Dima Bondarenko <dimon.bond.94@gmail.com>
2022-07-26 23:11:06 +0300

    set refs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+const refs = {
+  input: document.querySelector("#search-box"),
+  countryList: document.querySelector(".country-list"),
+  countryInfo: document.querySelector(".country-info")
+}
