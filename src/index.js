99de58ccdf6144224c1e63002ec06087b84cc337
Dima Bondarenko <dimon.bond.94@gmail.com>
2022-07-27 00:14:57 +0300

    Add exported fetch function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
+import fetchCountriesByName  from './js/fetchCountries.js';
 
 const DEBOUNCE_DELAY = 300;
 const refs = {
@@ -7,3 +8,5 @@ const refs = {
   countryList: document.querySelector(".country-list"),
   countryInfo: document.querySelector(".country-info")
 }
+
+
